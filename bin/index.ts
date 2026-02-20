e5b0e471bcda310633d49fcb3bcc33e9050f1bab
Justin <shpaw415@gmail.com>
2026-02-20 15:02:29 -0500

    up

--- bin/index.ts
+++ bin/index.ts
@@ -76,6 +76,16 @@ program
     "-m, --method <method>",
     "Initialization method (wrangler or git)",
   )
+  .requiredOption(
+    "-j, --jurisdiction <jurisdiction>",
+    'Jurisdiction for the server ["eu", "fedramp"]',
+    "eu",
+  )
+  .requiredOption(
+    "-l, --location <location>",
+    "Location for the server \nweur: Western Europe\neeur: Eastern Europe\napac: Asia Pacific\noc: Oceania\nwnam: Western North America\nenam: Eastern North America",
+    "enam",
+  )
   .option("-r, --repo <repo>", "Git repository URL for git initialization")
   .description("Initialize the OpenAuth Multitenant Server")
   .action(async (options) => {
@@ -83,20 +93,66 @@ program
 
     const method = options.method as "wrangler" | "git";
 
-    if (method === "git") {
-      await ensureGitExists();
-      if (!options.repo) {
-        console.error("Git repository URL is required for git initialization.");
-        process.exit(1);
-      }
-    }
-
     if (!validMethods.includes(method)) {
       console.error(
         `Invalid initialization method: ${method}. Valid methods are: ${validMethods.join(", ")}`,
       );
       process.exit(1);
     }
+
+    if (method === "git") {
+      await ensureGitExists();
+      if (!options.repo) {
+        console.error("Git repository URL is required for git initialization.");
+        process.exit(1);
+      }
+
+      const gitCreateResult = await execSync(
+        `git init && git remote add cloudflare ${options.repo}`,
+      );
+      if (gitCreateResult.stderr) {
+        console.error(
+          "Error initializing git repository:",
+          gitCreateResult.stderr,
+        );
+        process.exit(1);
+      }
+
+      const gitPushResult = await execSync(
+        `git push --set-upstream cloudflare main`,
+      );
+      if (gitPushResult.stderr) {
+        console.error(
+          "Error setting upstream and pushing to git:",
+          gitPushResult.stderr,
+        );
+        process.exit(1);
+      }
+    }
+
+    console.log("Generating wrangler.json configuration...");
+    const wranglerExempleFile = await Bun.file(
+      "./wrangler.example.json",
+    ).text();
+    const wranglerConfig = Bun.JSONC.parse(
+      wranglerExempleFile,
+    ) as typeof import("../wrangler.json");
+
+    wranglerConfig.d1_databases = [];
+
+    await Bun.write("./wrangler.json", JSON.stringify(wranglerConfig, null, 2));
+    console.log("wrangler.json configuration generated successfully!");
+
+    const createDBResult = await execSync(
+      `wrangler d1 create openauthster --binding AUTH_DB --update-config true --remote true --jurisdiction ${options.jurisdiction} --location ${options.location}`,
+    );
+
+    if (createDBResult.stderr) {
+      console.error("Error creating D1 database:", createDBResult.stderr);
+      process.exit(1);
+    }
+
+    console.log("D1 database created successfully!");
 
     const dbResult = await execSync(`wrangler d1 apply AUTH_DB`);
     if (dbResult.stderr) {
